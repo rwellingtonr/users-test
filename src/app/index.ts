050604c27125de4d052e30556560d36b344f1199
Wellington Leardini <wellington-158@hotmail.com>
2022-02-12 15:21:14 -0300

    add midleware to handle errors

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { NextFunction, Request, Response } from "express"
 import cors from "cors"
 import { createServer } from "http"
 import { router } from "../routes/routes"
@@ -9,6 +9,13 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(router)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	if (err instanceof Error) {
+		return res.status(400).json({ message: err.message })
+	}
+	return res.status(500).json({ status: "error", message: `Internal server error: ${err}` })
+})
 
 /*Server with http method*/
 const serverHttp = createServer(app)
